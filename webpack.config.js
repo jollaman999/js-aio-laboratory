7f02a1b54bd8feb8209074b05a049697d718bfc2
jollaman999 <admin@jollaman999.com>
2021-12-20 21:58:46 +0900

    Apply html-webpack-plugin
    - Add index.html template to public folder and remove from build destination folder.
    - Now, we can remove dist/index.html from .gitignore!

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: '/src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
+    filename: '[name]-[contenthash].js',
   },
   module: {
     rules: [
@@ -26,4 +28,10 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+  ],
 };
