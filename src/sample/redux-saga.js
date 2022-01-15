46cbe69e26d446ba6a09cf17d19bfd36864108b2
jollaman999 <admin@jollaman999.com>
2022-01-16 01:08:13 +0900

    Apply rootSaga and Use various redux-saga effects

--- src/sample/redux-saga.js
+++ src/sample/redux-saga.js
@@ -1,54 +1,72 @@
-import { createStore, applyMiddleware } from 'redux'
-import createSagaMiddleware from 'redux-saga'
-import { take, delay } from 'redux-saga/effects'
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { all, call, delay, put, take, takeEvery } from "redux-saga/effects";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider, useSelector } from "react-redux";
 
-const ASYNC_ACTION1 = 'ASYNC_ACTION1';
-const ASYNC_ACTION2 = 'ASYNC_ACTION2';
+const ASYNC_ACTION1 = "ASYNC_ACTION1";
+const ASYNC_ACTION2 = "ASYNC_ACTION2";
+const ASYNC_GET_COMMENT = "ASYNC_GET_COMMENT";
+const SET_COMMENT = "SET_COMMENT";
 
 const reducer = (state = 0, action) => {
-  console.log('REDUCER', state, action);
-  return action.payload;
-}
+  switch (action.type) {
+    case SET_COMMENT:
+      return { ...state, comment: action.payload }
+    default:
+      return state;
+  }
+};
 
 function* helloSaga() {
   while (true) {
-    console.log('START HELLO SAGA')
+    console.log("START HELLO SAGA");
     yield take(ASYNC_ACTION1);
     yield delay(1000);
-    console.log('END ASYNC_ACTION1')
+    console.log("END ASYNC_ACTION1");
     yield take(ASYNC_ACTION2);
     yield delay(1000);
-    console.log('END ASYNC_ACTION2')
+    console.log("END ASYNC_ACTION2");
   }
 }
 
+function* watchCommentSaga() {
+  yield takeEvery(ASYNC_GET_COMMENT, function* () {
+    const comment = yield call((id) => fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then(res =>
+        res.json()
+      ),
+      Math.floor(Math.random() * 10) + 1
+    );
+    yield put({ type: SET_COMMENT, payload: comment });
+  });
+}
+
+function* rootSaga() {
+  yield all([helloSaga(), watchCommentSaga()]);
+}
 
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(reducer, applyMiddleware(sagaMiddleware))
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
 
-sagaMiddleware.run(helloSaga);
+sagaMiddleware.run(rootSaga);
 
-// 처음 호출된 dispatch 에 의해서 호출된 delay 함수가 끝나기 전에
-// 다시 dispatch 가 호출되면 delay 함수는 한번만 실행된다.
-// 위에서 delay 함수에 1초 (1000ms) 를 부여 했기 때문에
-// 다음에 호출되는 함수는 1초 이상의 시간을 주어야 실행된다.
-setTimeout(() => store.dispatch({type: ASYNC_ACTION1}), 1000);
-setTimeout(() => store.dispatch({type: ASYNC_ACTION2}), 3000);
-setTimeout(() => store.dispatch({type: ASYNC_ACTION1}), 5000);
-setTimeout(() => store.dispatch({type: ASYNC_ACTION1}), 7000); // helloSaga wait ASYNC_ACTION2
-setTimeout(() => store.dispatch({type: ASYNC_ACTION2}), 9000);
+store.dispatch({ type: ASYNC_GET_COMMENT });
+store.subscribe(() => console.log(store.getState()))
 
-/*
-START HELLO SAGA
-REDUCER 0 {type: 'ASYNC_ACTION1'}
-END ASYNC_ACTION1
-REDUCER 0 {type: 'ASYNC_ACTION2'}
-END ASYNC_ACTION2
-START HELLO SAGA
-REDUCER 0 {type: 'ASYNC_ACTION1'}
-END ASYNC_ACTION1
-REDUCER 0 {type: 'ASYNC_ACTION1'}
-REDUCER 0 {type: 'ASYNC_ACTION2'}
-END ASYNC_ACTION2
-START HELLO SAGA
- */
+function Component() {
+  const comment = useSelector(state => state.comment);
+  // console.log('selector', user)
+  return (
+    <>
+      {comment?.name}
+    </>
+  )
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Component />
+  </Provider>,
+  document.getElementById('redux-saga')
+)
